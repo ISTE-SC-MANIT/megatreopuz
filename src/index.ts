83f4c2247f141b8fc9024d23a281ba0058cf1523
Your Name <you@example.com>
2020-01-17 18:25:34 +0530

    feat(deployment): heroku set

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("its working");
+});
+
+if (process.env.PORT) {
+  console.log("Listening on port", process.env.PORT);
+  app.listen(process.env.PORT);
+}
